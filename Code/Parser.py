f4bd970595538552316c277d0f166243f6676e1b
Alex <alexandrumoldoveanu97@gmail.com>
2024-06-20 17:53:31 +0300

    Improve Parser pylint note from 6.05 to 8.12

--- Code/Parser.py
+++ Code/Parser.py
@@ -1,15 +1,14 @@
+"""This module parses log files, saves results, and manages project files."""
+
 import re
 import sys
 import os
 import subprocess
 
-project_name = sys.argv[1]
-check = sys.argv[2]
-
 def parse_log_file(file_path):
+    """Parses the log file and extracts errors, warnings, information, style issues, and notes."""
     try:
-        with open(file_path, 'r') as file:
-            content = file.read()
+        with open(file_path, 'r', encoding='utf-8') as file:
             errors = []
             warnings = []
             information = []
@@ -20,20 +19,19 @@ def parse_log_file(file_path):
             information_pattern = re.compile(r'\binformation\b', re.IGNORECASE)
             style_pattern = re.compile(r'\bstyle\b', re.IGNORECASE)
             note_pattern = re.compile(r'\bnote\b', re.IGNORECASE)
-            
-            with open(file_path, 'r') as file:
-                for line in file:
-                    if error_pattern.search(line):
-                        errors.append(line.strip())
-                    elif warning_pattern.search(line):
-                        warnings.append(line.strip())
-                    elif information_pattern.search(line):
-                        information.append(line.strip())
-                    elif style_pattern.search(line):
-                        style.append(line.strip())
-                    elif note_pattern.search(line):
-                        notes.append(line.strip())
-            
+
+            for line in file:
+                if error_pattern.search(line):
+                    errors.append(line.strip())
+                elif warning_pattern.search(line):
+                    warnings.append(line.strip())
+                elif information_pattern.search(line):
+                    information.append(line.strip())
+                elif style_pattern.search(line):
+                    style.append(line.strip())
+                elif note_pattern.search(line):
+                    notes.append(line.strip())
+
             return errors, warnings, information, style, notes
     except FileNotFoundError:
         print(f"Error: The file at {file_path} does not exist.")
@@ -43,6 +41,7 @@ def parse_log_file(file_path):
         return [], [], [], [], []
 
 def save_results(errors, warnings, information, style, notes, output_file):
+    """Saves the parsed results to an output file."""
     if os.path.exists(output_file):
         old_output_file = output_file.replace(".txt", "_old.txt")
 
@@ -50,35 +49,36 @@ def save_results(errors, warnings, information, style, notes, output_file):
             os.remove(old_output_file)
 
         os.rename(output_file, old_output_file)
-        
+
     if not errors and not warnings and not information and not style and not notes:
         print(f"No results to save in {output_file}")
         return
 
-    with open(output_file, 'w') as file:
-        file.write("Errors ({}):\n".format(len(errors)))
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(f"Errors ({len(errors)}):\n")
         for error in errors:
             file.write(error + '\n')
 
-        file.write("\nWarnings ({}):\n".format(len(warnings)))
+        file.write(f"\nWarnings ({len(warnings)}):\n")
         for warning in warnings:
             file.write(warning + '\n')
 
-        file.write("\nInformation ({}):\n".format(len(information)))
+        file.write(f"\nInformation ({len(information)}):\n")
         for info in information:
             file.write(info + '\n')
 
-        file.write("\nStyle ({}):\n".format(len(style)))
+        file.write(f"\nStyle ({len(style)}):\n")
         for st in style:
             file.write(st + '\n')
 
-        file.write("\nNotes ({}):\n".format(len(notes)))
+        file.write(f"\nNotes ({len(notes)}):\n")
         for note in notes:
             file.write(note + '\n')
 
 def print_file_contents(file_path):
+    """Prints the contents of a file."""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             contents = file.read()
             print(contents)
     except FileNotFoundError:
@@ -87,11 +87,15 @@ def print_file_contents(file_path):
         print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
-    input_file_path = project_name + '\\result.txt'
+    """Main entry point of the script."""
+    project_name = sys.argv[1]
+    check = sys.argv[2]
+
+    input_file_path = os.path.join(project_name, 'result.txt')
     current_directory = os.getcwd()
     target_dir = os.path.abspath(os.path.join(current_directory, 'Result'))
-    output_file_path = target_dir + '\\'+ project_name + '_parsed_results.txt'
+    output_file_path = os.path.join(target_dir, f'{project_name}_parsed_results.txt')
 
     errors, warnings, information, style, notes = parse_log_file(input_file_path)
     save_results(errors, warnings, information, style, notes, output_file_path)
-    subprocess.run(['python', 'Validate.py', output_file_path])
+    subprocess.run(['python', 'Validate.py', output_file_path], check=True)
